71dd4072a34d84acf12925d45db0d83259b39776
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-08-11 14:45:03 -0500

    added collection variable, addToCollection function, and record1 object

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,13 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+let record1 = {
+    title: 'This Empty Northern Hemisphere',
+    artist: 'Gregory Alan Isakov',
+    yearPublished: 2009,
+}
+
+
+function addToCollection (title, artist, yearPublished){
+
+}
