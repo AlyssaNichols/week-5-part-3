22709960a1813ebabd4c506a419b6d6a74d0ff3d
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-08-15 11:59:33 -0500

    added 6 albums to collection and created teh showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -15,6 +15,25 @@ return newRecord;
 }
 
 
+console.log('Adding another album to the collection', addToCollection('This Empty Northern Hemisphere', 'Gregory Alan Isakov', 2009));
+console.log('Adding another album to the collection', addToCollection('The Weatherman', 'Gregory Alan Isakov', 2013));
+console.log('Adding another album to the collection', addToCollection('Rumours', 'Fleetwood Mac', 1977));
+console.log('Adding another album to the collection', addToCollection('Bella Donna', 'Stevie Nicks', 1981));
+console.log('Adding another album to the collection', addToCollection('The Wild Heart', 'Stevie Nicks', 1983));
+console.log('Adding another album to the collection', addToCollection('Sound & Color', 'Alabama Shakes', 2015));
+
+console.log('My collection so far is:', collection);
+
+
+function showCollection(albumArray) {
+  console.log(`Number of items in the collection: ${albumArray.length}`);
+  for (const album of albumArray) {
+    console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+  }
+}
+
+showCollection(collection);
+
 
 
 
