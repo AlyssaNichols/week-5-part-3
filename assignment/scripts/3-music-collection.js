a6ccedced2a8e5e24d95b83aa04323a16bf2cdc1
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-08-16 15:08:33 -0500

    didn't change anything of importance, just went through everything again for practice and added some comments

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,5 +1,4 @@
 console.log('***** Music Collection *****')
-
 let collection = [];
 
 
@@ -15,11 +14,15 @@ collection.push(newAlbum);
 return newAlbum;
 }
 
+addToCollection('Sadnecessary', 'Milky Chance', 2013);
+
+//added 1 album to the collection and checking it if worked
+console.log(collection);
+
 // Adding albums to collection using the addToCollection function and inputting the proper parameters to replace the placeholders.
 
 console.log('Adding another album to the collection', addToCollection('This Empty Northern Hemisphere', 'Gregory Alan Isakov', 2009));
 console.log('Adding another album to the collection', addToCollection('The Weatherman', 'Gregory Alan Isakov', 2013));
-console.log('Adding another album to the collection', addToCollection('Sadnecessary', 'Milky Chance', 1977));
 console.log('Adding another album to the collection', addToCollection('Bella Donna', 'Stevie Nicks', 1981));
 console.log('Adding another album to the collection', addToCollection('The Wild Heart', 'Stevie Nicks', 1983));
 console.log('Adding another album to the collection', addToCollection('Sound & Color', 'Alabama Shakes', 2015));
@@ -27,7 +30,7 @@ console.log('Adding another album to the collection', addToCollection('Sound & C
 console.log('My collection so far is:', collection.length, collection);
 
 
-//function to show the collection
+//function to show the collection- don't need to console.log the function because console logging is within the function.
 
 function showCollection (array) {
   console.log('Number of items in the collection:', array.length);
@@ -77,10 +80,9 @@ function findByArtist(artist){
  
 
 // searching 2 artists that I have in the collection and 1 that isn't to make sure the function is working properly.
-console.log('Albums by Stevie Nicks', findByArtist ('Stevie Nicks'));
-console.log('Albums by Caamp', findByArtist ('Caamp'));
-console.log('Albums by Milky Chance', findByArtist ('Milky Chance'));
-
+console.log('Albums in collection by Stevie Nicks', findByArtist ('Stevie Nicks'));
+console.log('Albums in collection by Caamp', findByArtist ('Caamp'));
+console.log('Albums in collection by Milky Chance', findByArtist ('Milky Chance'));
 
 
 
