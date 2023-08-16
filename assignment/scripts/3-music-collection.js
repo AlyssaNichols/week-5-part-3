93b18bc25fae7ba332e67d758dd7134ca0e2f21d
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-08-16 10:21:43 -0500

    added showCollectino function for of option and just going over things again for practice

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,6 +2,7 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+
 function addToCollection (title, artist, yearPublished){
     const newRecord = {
         title: title,
@@ -14,18 +15,67 @@ collection.push(newRecord);
 return newRecord;
 }
 
+// Adding albums to collection using the addToCollection function and inputting the proper parameters to replace the placeholders.
 
 console.log('Adding another album to the collection', addToCollection('This Empty Northern Hemisphere', 'Gregory Alan Isakov', 2009));
 console.log('Adding another album to the collection', addToCollection('The Weatherman', 'Gregory Alan Isakov', 2013));
-console.log('Adding another album to the collection', addToCollection('Rumours', 'Fleetwood Mac', 1977));
+console.log('Adding another album to the collection', addToCollection('Sadnecessary', 'Milky Chance', 1977));
 console.log('Adding another album to the collection', addToCollection('Bella Donna', 'Stevie Nicks', 1981));
 console.log('Adding another album to the collection', addToCollection('The Wild Heart', 'Stevie Nicks', 1983));
 console.log('Adding another album to the collection', addToCollection('Sound & Color', 'Alabama Shakes', 2015));
 
-console.log('My collection so far is:', collection);
+console.log('My collection so far is:', collection.length, collection);
 
 
-function showCollection(albumArray) {
+//function to show the collection
+
+function showCollection (array) {
+  console.log('Number of items in the collection:', array.length);
+  for (i = 0; i < array.length; i++){
+    const album = array[i];
+    console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+}
+}
+
+showCollection(collection);
+
+// This is the same function as above, just with a for of loop 
+/*
+function showCollection(array) {
+  console.log('Number of items in the collection:', array.length);
+  for (const album of array) {
+    console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+  }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function showCollection(albumArray) {
   console.log(`Number of items in the collection: ${albumArray.length}`);
   for (const album of albumArray) {
     console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
@@ -50,19 +100,11 @@ console.log('Albums by Stevie Nicks', findByArtist('Stevie Nicks'));
 
 console.log('Albums by Caamp', findByArtist('Caamp'));
 
-console.log('Albums by Fleetwood Mac', findByArtist('Fleetwood Mac'));
+console.log('Albums by Milky Chance', findByArtist('Milky Chance'));
 
 
-
-
-
-
-/* 
 
 const fullName = (`${me.firstName} ${me.lastName}`);
 
-CAN CREATE NEW VARIABLES TO USE FOR OBJECT LOOPS AND FUNCTIONS
-
-COME BACK TO THIS!!! 
 */
 
